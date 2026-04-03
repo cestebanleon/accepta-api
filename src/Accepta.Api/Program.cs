b8c7caf1cf43ba5e11596611dd64ef118cee6edd
cestebanleon <cestebanleon@gmail.com>
2026-04-03 17:52:36 +0100

    feat: enable swagger in production

--- src/Accepta.Api/Program.cs
+++ src/Accepta.Api/Program.cs
@@ -9,11 +9,8 @@ builder.Services.AddAWSLambdaHosting(LambdaEventSource.RestApi);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
